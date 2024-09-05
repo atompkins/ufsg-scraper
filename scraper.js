755658a803ffa2ae232305f207e8a6bb1fb9ec3d
atompkins <atompkins@users.noreply.github.com>
2024-09-05 19:41:28 +0100

    debug-morph-2

--- scraper.js
+++ scraper.js
@@ -1,6 +1,11 @@
 const init = require('./src/init');
 const stdout = require('./src/stdout');
 
+stdout('fetch', typeof fetch);
+stdout('Request', typeof Request);
+stdout('Response', typeof Response);
+stdout('ReadableStream', typeof ReadableStream);
+
 init()
   .then(() => stdout('Finished...'))
   .catch((e) => stdout(e));
